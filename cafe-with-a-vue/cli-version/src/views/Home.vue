35480587e55ea88f03a3a3216809525009db3e12
MaximeLeBonniec <maxime.lebonniec@hotmail.com>
2022-03-18 11:44:29 +0100

    feat: moove application datas from components and views to vuex store

--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -34,6 +34,8 @@
 
 <script>
 import MenuItem from "../components/MenuItem"
+import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
 	name: "Home",
@@ -42,48 +44,15 @@ export default {
 	},
 	data() {
 		return {
-			restaurantName: "La belle vue",
-			shoppingCart: 0,
-			simpleMenu: [
-				{
-					name: "Croissant",
-					image: {
-						source: "/images/crossiant.jpg",
-						alt: "Un croissant"
-					},
-					inStock: true,
-					quantity: 1,
-					price: 2.99
-				},
-				{
-					name: "Baguette de pain",
-					image: {
-						source: "/images/french-baguette.jpeg",
-						alt: "Quatre baguettes de pain"
-					},
-					inStock: true,
-					quantity: 1,
-					price: 3.99
-				},
-				{
-					name: "Éclair",
-					image: {
-						source: "/images/eclair.jpg",
-						alt: "Éclair au chocolat"
-					},
-					inStock: false,
-					quantity: 1,
-					price: 4.99
-				}
-			]
 		}
 	},
 	computed: {
-		copyright() {
-			const currentYear = new Date().getFullYear()
-
-			return `Copyright ${this.restaurantName} ${currentYear}`
-		}
+		...mapGetters(['copyright']),
+		...mapState({
+            shoppingCart: 'shoppingCart',
+            restaurantName: 'restaurantName',
+            simpleMenu: 'simpleMenu'
+        })
 	},
 	methods: {
 		addToShoppingCart(amount) {
